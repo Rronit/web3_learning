c81036b31d9c7da20b4614a07944b78ecd26a684
Rronit <rronit2015@gmail.com>
2022-05-08 20:59:14 +0530

    feat: html interact with smart contract basic DApp

---
+++ first_dapp/constants.js
@@ -0,0 +1,32 @@
+const contractAddress="0x5cc64e893fd551397ff6f01dbb45873fa7ee6dbc";
+const contractABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_mood",
+				"type": "string"
+			}
+		],
+		"name": "setMood",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function",
+    "constant":false
+	},
+	{
+		"inputs": [],
+		"name": "getMood",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function",
+    "constant":true
+	}
+];
+
